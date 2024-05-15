80966a9813c9ab558a6b0f7773bb4cc8846917f9
Danilo Santos <danilondosantos@gmail.com>
2024-05-15 01:07:27 -0300

    refactor: :recycle: configured UserFactory

--- app/Domain/User/Models/User.php
+++ app/Domain/User/Models/User.php
@@ -3,6 +3,9 @@
 namespace App\Domain\User\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -44,4 +47,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected static function newFactory(): Factory
+    {
+        return UserFactory::new();
+    }
 }
