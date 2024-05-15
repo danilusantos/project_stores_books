f81cb84ab58da43ebd5061e9aa617a8261a22598
Danilo Santos <danilondosantos@gmail.com>
2024-05-15 00:14:51 -0300

    refactor: :recycle: applied versioning to api routes

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,19 @@
 <?php
 
-use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\v1\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::prefix('v1')
+    ->group(function () {
+        // Routes accessible only when authenticated
+        Route::middleware('auth:sanctum')->group(function () {
+            // Logout route
+            Route::post('/logout', [AuthController::class, 'logout']);
+        });
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
-
-Route::middleware('guest')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-});
+        // Routes accessible only when guest (not authenticated)
+        Route::middleware('guest')->group(function () {
+            // Login route
+            Route::post('/login', [AuthController::class, 'login']);
+        });
+    });
